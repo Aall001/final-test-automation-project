4fdeaf34c277e3cbcebceca3667e192f74bc74c9
Alena_Eshcheva <126786440+Aall001@users.noreply.github.com>
2023-07-23 20:25:39 +0700

    Update base_page.py

--- project_final/pages/base_page.py
+++ project_final/pages/base_page.py
@@ -6,20 +6,18 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
         self.base_url = 'https://clck.ru/354zU3'
+
     def go_to_sait(self):
+        """ Переход на базовую страницу Авторизации """
         return self.driver.get(self.base_url)
 
     def find_element(self, locator, time=10):
+        """ Поиск нужного элемента"""
         return WebDriverWait(self.driver, time).until(ES.presence_of_element_located(locator), message=f"Not find {locator}")
 
     def find_elements(self, locator, time=10):
+        """ Поиск нескольких элементов"""
         return WebDriverWait(self.driver, time).until(ES.presence_of_all_elements_located(locator))
-
-
-#
-
-
-
 
 
 
